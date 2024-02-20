2d63fcee537bec9ba042693c85a9c35403614fc8
zootedb0t <05kraken@proton.me>
2024-02-20 22:46:48 +0530

    merge(upstream): https://git.suckless.org/st/commit/a3f7420310be0fd778ef9fe2abf20edc2d8dc81a.html

--- st.c
+++ st.c
@@ -1805,6 +1805,7 @@ csihandle(void)
 		}
 		break;
 	case 'S': /* SU -- Scroll <n> line up */
+		if (csiescseq.priv) break;
 		DEFAULT(csiescseq.arg[0], 1);
 		tscrollup(term.top, csiescseq.arg[0], 0);
 		break;
