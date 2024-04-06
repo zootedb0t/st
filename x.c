991c82648ddc9159bc1d7f4fa57b438a123b963d
zootedb0t <05kraken@proton.me>
2024-04-06 19:59:14 +0530

    merge(upstream): https://git.suckless.org/st/commit/497a75638291454875ba1ec8d484c7f3d6f41d66.html#h0-0-4

--- x.c
+++ x.c
@@ -1877,6 +1877,9 @@ xseticontitle(char *p)
 	XTextProperty prop;
 	DEFAULT(p, opt_title);
 
+	if (p[0] == '\0')
+		p = opt_title;
+
 	if (Xutf8TextListToTextProperty(xw.dpy, &p, 1, XUTF8StringStyle,
 	                                &prop) != Success)
 		return;
@@ -1890,6 +1893,9 @@ xsettitle(char *p)
 {
 	XTextProperty prop;
 	DEFAULT(p, opt_title);
+
+	if (p[0] == '\0')
+		p = opt_title;
 
 	if (Xutf8TextListToTextProperty(xw.dpy, &p, 1, XUTF8StringStyle,
 	                                &prop) != Success)
