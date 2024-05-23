27ef88defa456f515aac4fdd02e48472d1d5a9a3
zootedb0t <05kraken@proton.me>
2024-05-23 21:01:46 +0530

    add upstream patch https://git.suckless.org/st/commit/5dbcca49263be094fc38159c297458ae323ef647.html

--- st.c
+++ st.c
@@ -1206,6 +1206,7 @@ csiparse(void)
 {
 	char *p = csiescseq.buf, *np;
 	long int v;
+	int sep = ';'; /* colon or semi-colon, but not both */
 
 	csiescseq.narg = 0;
 	if (*p == '?') {
@@ -1223,7 +1224,9 @@ csiparse(void)
 			v = -1;
 		csiescseq.arg[csiescseq.narg++] = v;
 		p = np;
-		if (*p != ';' || csiescseq.narg == ESC_ARG_SIZ)
+		if (sep == ';' && *p == ':')
+			sep = ':'; /* allow override to colon once */
+		if (*p != sep || csiescseq.narg == ESC_ARG_SIZ)
 			break;
 		p++;
 	}
